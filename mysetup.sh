a98af117cdba10d8dd9bc80228fbe7c170999441
Andrew Le <136618059+hieultph@users.noreply.github.com>
2025-07-22 09:06:39 +0700

    Update mysetup.sh

--- mysetup.sh
+++ mysetup.sh
@@ -6,7 +6,7 @@ set -e
 # Download and install Miniconda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
-~/miniconda3/bin/conda init bash
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
 conda init bash
 source ~/.bashrc
 
